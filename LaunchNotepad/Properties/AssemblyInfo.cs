0c704c89531e93428dcded4bddc208613df70f27
motovmp1 <vinicius.mpinho@gmail.com>
2020-11-18 16:26:01 +0000

    first commit

---
+++ LaunchNotepad/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LaunchNotepad")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaunchNotepad")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36512aae-1d27-4a9d-8f5a-0fce6de1d59f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LaunchNotepad/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Winium;
+using System.Threading;
+using System.Security.Policy;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+
+namespace LaunchNotepad
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void LaunchNotepadAndWriteSomething() 
+        {
+            DesktopOptions options = new DesktopOptions();
+            options.ApplicationPath = (@"C:\Windows\System32\notepad.exe");
+
+            WiniumDriver driver = new WiniumDriver(@"C:\Users\User01\Downloads\Winium.Desktop.Driver", options);
+
+            Thread.Sleep(2000);
+            driver.FindElementByClassName("Edit").SendKeys("Eliza");
+            Thread.Sleep(2000);
+            driver.FindElementByName("File").Click();
+            Thread.Sleep(2000);
+            driver.FindElementById("Close").Click();
+            Thread.Sleep(2000);
+            driver.FindElementByName("Don't Save").Click();
+
+
+
+
+
+            
+            
+
+
+
+
+        }
+    }
+}
